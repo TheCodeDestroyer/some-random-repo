f6c6bd2ae494f41e477f8d46342c55bf83c0c88c
Nace Logar <nace.logar@iryo.io>
2022-10-21 14:38:13 +0200

    Create error middleware

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,21 @@
+const logger = require('../utils/logger');
+const ApiError = require('../utils/ApiError');
+
+// Unused param next needs to remain
+const errorMiddleware = function (err, req, res, next) {
+  if (err && err.stack) {
+    logger.error(err.stack);
+  }
+
+  res.contentType('application/json');
+
+  if (err instanceof ApiError) {
+    const { statusCode, message } = err;
+    res.status(statusCode).json({ message });
+    return;
+  }
+
+  res.status(500).send({ message: 'Something went wrong!' });
+};
+
+module.exports = errorMiddleware;
